444324583a6a7d143b0fda50feed30264f973b80
flutterkage2k <jinheesung1@gmail.com>
2023-05-24 23:22:55 +0900

    5-4 complete
    fromjson, frommodel create

--- lib/restaurant/component/restaurant_card.dart
+++ lib/restaurant/component/restaurant_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttermidcodefactorywithapi230426/common/const/colors.dart';
+import 'package:fluttermidcodefactorywithapi230426/restaurant/model/restaurant_model.dart';
 
 class RestaurantCard extends StatelessWidget {
   final Widget image;
@@ -20,6 +21,23 @@ class RestaurantCard extends StatelessWidget {
     required this.deliveryFee,
     required this.ratings,
   }) : super(key: key);
+
+  factory RestaurantCard.fromModel({
+    required RestaurantModel model,
+  }) {
+    return RestaurantCard(
+      image: Image.network(
+        model.thumbUrl,
+        fit: BoxFit.cover,
+      ),
+      name: model.name,
+      tags: model.tags,
+      ratingsCount: model.ratingsCount,
+      deliveryTime: model.deliveryTime,
+      deliveryFee: model.deliveryFee,
+      ratings: model.ratings,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
